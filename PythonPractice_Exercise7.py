c4a769ca5f206cbc9dc47168ef4a9e9d1dab7a6e
jack96harrison <jackharrison1996@me.com>
2017-10-03 12:31:59 +0100

    PracticePython_Exercise7

---
+++ PythonPractice_Exercise7.py
@@ -0,0 +1,10 @@
+"""
+Take a variable of a list e.g a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Write one line of python that makes a new list with only the even elements
+"""
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+b = a[1::2]
+
+print(a)
+print(b)
